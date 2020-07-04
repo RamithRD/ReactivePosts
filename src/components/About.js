51da2397cfae244d7a337acb14360eba26670210
RamithR <ramithrandeniya@gmail.com>
2020-07-04 15:10:54 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>lorem seseg seg sh srh srh srh </p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = (props) => {
+      
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>lorem seseg seg sh srh srh srh </p>
+        </div>
+    )
+}
+
+export default Contact
